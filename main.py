077651621eb724fb56e4409dae40aefadce4cb93
LeonamAlmeida <leonamvictor12@gmail.com>
2023-08-19 12:47:33 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,168 @@
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+
+Graph = nx.Graph()
+
+# retorna umm dicionário associando cada político com seu respectivo partido
+def read_file_by_political_party(file_name, p_party):
+    selected_parties = set(p_party.split(" "))
+    politicians_by_party = {}
+    with open(file_name, 'r', encoding = 'utf-8') as file:
+        i = 0
+        for line in file:
+            i += 1
+            line_content = line.strip().split(";")
+            politician_name = line_content[0]
+            party_name = line_content[1]
+            if (not selected_parties or party_name in selected_parties) or p_party == "":
+                if politician_name not in politicians_by_party:
+                    politicians_by_party[politician_name] = []
+                politicians_by_party[politician_name].append(party_name)
+        file.close()
+    return politicians_by_party
+
+# cria o grafo a partir do txt do grafo e os políticos por partido
+def read_file(G, file_name, politicians_by_party):
+    with open(file_name, 'r', encoding = 'utf-8') as file:
+        i = 0
+        for line in file:
+            i += 1
+            line_content = line.strip().split(";")
+            u = line_content[0]
+            v = line_content[1]
+            w = int(line_content[2])
+            if u in politicians_by_party and v in politicians_by_party:
+                # Concatena os partidos com os nomes dos deputados para criar os nós
+                u_with_party = f"({', '.join(politicians_by_party[u])}) {u}"
+                v_with_party = f"({', '.join(politicians_by_party[v])}) {v}"
+                G.add_edge(u_with_party, v_with_party, weight=w)
+        file.close()
+
+# retorna um dicionário com o número de voto de cada político, o que é útil para a normalização do grafo
+def read_votes_data(file_path, p_party1):
+    selected_parties = set(p_party1.split(" "))
+    votes_dict = {}
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            fields = line.strip().split(';')
+            if len(fields) == 3:
+                politician_name, p_party, votes = fields
+                if p_party in selected_parties or p_party1 == "":
+                    politician_name = f"({p_party}) {politician_name}"
+                    votes_dict[politician_name] = int(votes)
+    return votes_dict
+
+# normaliza o grafo 
+def normalize_graph(G, votes_dict):
+    # campo 'data' é um atributo que é utilizado para acessar especificadamente o campo de peso de cada aresta
+    for u, v, data in G.edges(data=True):
+        weight = data['weight']
+        min_votes = min(votes_dict[u], votes_dict[v])
+        normalized_weight = weight / min_votes
+        # normalized_weight = round(normalized_weight, 3) # arredondamento para 3 casas decimais
+        G.add_edge(u, v, weight=normalized_weight)
+
+# threshold de 0.9 será utilizado
+def threshold(G, threshold):
+    # criação de uma lista para remoção de arestas, caso removesse diretamente do grafo, o grafo seria modificado durante a iteração
+    # erro de runtime -> RuntimeError: dictionary changed size during iteration 
+    edges_to_remove = []
+    for u, v, data in G.edges(data=True):
+        if data['weight'] < threshold:
+            edges_to_remove.append((u, v))
+    # aqui usamos remove_edges_from para remover as arestas da lista criada anteriormente
+    G.remove_edges_from(edges_to_remove)
+
+def inversion_of_weights(G):
+    for u, v, data in G.edges(data=True):
+        data['weight'] = 1 - data['weight']
+
+# vizualizar o grafo
+def write_graph_to_file(graph, file_path):
+    with open(file_path, 'w', encoding='utf-8') as file:
+        for u, v, data in graph.edges(data=True):
+            file.write(f"{u};{v};{data['weight']}\n")
+
+def create_betwenness(Graph, year, p_party):
+    
+    betweenness_centrality = nx.betweenness_centrality(Graph)
+    
+    nodes, centralities = zip(*sorted(betweenness_centrality.items(), key=lambda item: item[1]))
+    
+    # Plotar o grafico de barras
+    plt.figure(figsize=(17, 15))
+    plt.bar(nodes, centralities)
+    plt.xlabel("Deputados")
+    plt.ylabel("Betweenness")
+    plt.title("Medida de Centralidade")
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+    
+    # Salvando
+    plt.savefig(f"betwenness_/betwenness_{year}_{p_party}.png")
+    
+def create_heatmap(Graph, year, p_party):
+    # Chamando novamente (3 linhas abaixo) para que o threshold e inversão não afete o heatmap
+    read_file(Graph, graph_path, read_file_by_political_party(politicians_path, p_party))
+    print(Graph)
+    normalize_graph(Graph, read_votes_data(politicians_path, p_party))
+    
+    # Criar uma matriz de adjacência com os pesos das arestas
+    num_nodes = len(Graph.nodes())
+    adj_matrix = np.zeros((num_nodes, num_nodes))
+
+    # Preencher a matriz com os pesos das arestas do grafo
+    for u, v, data in Graph.edges(data=True):
+        u_idx = list(Graph.nodes()).index(u)
+        v_idx = list(Graph.nodes()).index(v)
+        adj_matrix[u_idx][v_idx] = data['weight']
+        adj_matrix[v_idx][u_idx] = data['weight']  # A matriz é simétrica para grafos não direcionados
+
+    # Criar o heatmap usando o Matplotlib
+    plt.figure(figsize=(25, 21))
+    plt.imshow(adj_matrix, cmap='hot', origin='upper')
+    plt.colorbar(label='Peso das Arestas')
+    plt.xticks(ticks=np.arange(num_nodes), labels=list(Graph.nodes()), rotation=45, ha = 'right')
+    plt.yticks(ticks=np.arange(num_nodes), labels=list(Graph.nodes()))
+    plt.tight_layout()
+    plt.savefig(f"heatmap_/heatmap_{year}_{p_party}.png")
+
+def create_graph(Graph, year, p_party):
+    
+    nodes_to_remove = [node for node in Graph.nodes() if Graph.degree(node) == 0]
+
+    # Remover os nós com grau zero
+    for node in nodes_to_remove:
+        Graph.remove_node(node)
+
+    # Conf. layout do grafo
+    pos = nx.spring_layout(Graph)
+
+    fig, ax = plt.subplots(figsize=(25, 21))  # (largura, altura)
+    
+    # Desenha o grafo
+    nx.draw(Graph, pos, with_labels=True, node_size=400, node_color='skyblue', font_size=10, font_color='black', font_weight='bold')
+
+    plt.savefig(f"graph_/graph_{year}_{p_party}.png")
+
+if __name__ == '__main__':
+    year = input("Informe o ano a considerar ( de 2001 a 2023) : ")
+    p_party = input("Informe os partidos a analisar , separados por espaço ( ex . PT MDB PL ) : ")
+    threshold_idx = float(input("Informe o percentual mínimo de concordância ( threshold ) ( ex . 0.9) : "))
+    
+    graph_path = "datasets/graph" + year + ".txt"
+    politicians_path = "datasets/politicians" + year + ".txt"
+    
+    read_file(Graph, graph_path, read_file_by_political_party(politicians_path, p_party))
+    print(Graph)
+    normalize_graph(Graph, read_votes_data(politicians_path, p_party))
+    write_graph_to_file(Graph, "normalized_graph.txt")
+    threshold(Graph, threshold_idx)
+    write_graph_to_file(Graph, "normalized_graph_threshold.txt")
+    inversion_of_weights(Graph)
+    write_graph_to_file(Graph, "inverted_normalized_graph_threshold.txt")
+    
+    create_betwenness(Graph, year, p_party)
+    create_graph(Graph, year, p_party)
+    create_heatmap(Graph, year, p_party)
---
+++ test_runner.py
@@ -0,0 +1,29 @@
+import subprocess
+
+def run_test(year, p_party, threshold_idx):
+    cmd = f'python main.py'
+    input_data = f'{year}\n{p_party}\n{threshold_idx}\n'
+    
+    result = subprocess.run(cmd, input=input_data, text=True, shell=True, capture_output=True)
+    
+    print(result.stdout)
+    print(result.stderr)
+
+# Lista de combinações preestabelecidas de entradas
+# for i in range(2002, 2023):
+#     test_cases = [
+#     (i, '', '0.9'),
+# ]
+   
+test_cases = [
+    ('2023', 'PSOL PT', '0.9'),
+    ('2023', 'PT PL', '0.9'),
+    ('2023', 'PSD PT', '0.9'),
+    ('2023', 'MDB PT', '0.9'),
+    ('2023', '', '0.9'), 
+]
+
+if __name__ == '__main__':
+    for year, p_party, threshold_idx in test_cases:
+        print(f"Running test with year={year}, p_party={p_party}, threshold_idx={threshold_idx}")
+        run_test(year, p_party, threshold_idx)
